385a25def7bd3ddeb420aa7023e364ba860bd85b
albertseiler <113704524+albertseiler@users.noreply.github.com>
2023-02-15 23:42:05 +0100

    .

---
+++ script.js
@@ -0,0 +1,6 @@
+//Javascript
+const bill_input = document.querySelector('.inputbill')
+const people_input = document.querySelector('.inputpeople')
+const tip_percentage = document.querySelector('.tippercentage')
+const tip_sum = document.querySelector('.dollaramount one')
+const total_sum = document.querySelector('.dollaramount two')
