9a0665bb248ff185f185c9c3b28e7328819924e7
albertseiler <113704524+albertseiler@users.noreply.github.com>
2023-02-16 11:48:10 +0100

    .

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 //Javascript
-const bill_input = document.querySelector('.inputbill')
-const people_input = document.querySelector('.inputpeople')
-const tip_percentage = document.querySelector('.tippercentage')
-const tip_sum = document.querySelector('.dollaramount one')
-const total_sum = document.querySelector('.dollaramount two')
+const billInput = document.querySelector('.inputbill')
+const peopleInput = document.querySelector('.inputpeople')
+const tipPercentage = document.querySelector('.tippercentage')
+const tipSum = document.querySelector('.dollaramount one')
+const totalSum = document.querySelector('.dollaramount two')
+
+let billNumber = parseInt(billInput.value)
+
+let buttonValue = 0;
+
+let peopleNumber = parseInt(peopleInput.value)
+
+tipPercentage.forEach(button => {
+    buttonValue = parseFloat(button.value)
+    if (billNumber == 0 || isNaN(billNumber) || billNumber == Infinity || peopleNumber == 0 || peopleNumber == "") {
+        return
+    } else {
+        calcTips()
+    }
+});
+
+billInput.addEventListener("input", () => {
+    billNumber = parseFloat(billInput.value)
+
+    if (billNumber == 0 || isNaN(billNumber) || billNumber == Infinity || peopleNumber == 0 || peopleNumber == "") {
+        return
+    } else {
+        calcTips()
+    }
+})
