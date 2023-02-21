16019c56a37a505e76d3753eb98cfb1ea087fac3
albertseiler <113704524+albertseiler@users.noreply.github.com>
2023-02-21 19:41:38 +0100

    .

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ const peopleInput = document.querySelector('.inputpeople')
 const tipSum = document.getElementById("tip-amount")
 const totalSum = document.getElementById("total-amount")
 const tips = document.querySelectorAll(".tippercentage")
+const resetBtn = document.querySelector(".reset")
 
+
+//ADD EVENT LISTENERS
 billInput.addEventListener("input", billInputFun);
 peopleInput.addEventListener("input", peopleInputFun);
 tips.forEach(function (val) {
     val.addEventListener('click', handleClick)
 })
+resetBtn.addEventListener('click', handleRemove)
 
 billInput.value = "0.0"
 peopleInput.value = "1"
@@ -20,6 +24,7 @@ let billValue = 0.0;
 let peopleValue = 1;
 let tipValue = 0.15;
 
+//FUNCTIONS
 function billInputFun() {
     billValue = parseFloat(billInput.value);
     calculateTip();
@@ -43,10 +48,21 @@ function handleClick(event) {
 
 function calculateTip() {
     if (peopleValue >= 1) {
-        let tipSum = (billValue * tipValue) / peopleValue;
-        let totalSum = (billValue * tipSum) / peopleValue;
-        tipSum.innerHTML = "$" + tipSum.toFixed(2);
-        totalSum.innerHTML = "$" + totalSum.toFixed(2);
-        console.log((billValue * tipValue) / peopleValue)
+        let tipAmount = (billValue * tipValue) / peopleValue;
+        let total = (billValue + tipAmount) / peopleValue;
+        tipSum.innerHTML = "$" + tipAmount.toFixed(2);
+        totalSum.innerHTML = "$" + total.toFixed(2);
+        console.log((billValue * tipValue) / peopleValue);
+        console.log((billValue + tipSum) / peopleValue);
     }
+}
+
+function handleRemove(event) {
+    tipAmount = 0.00;
+    total = 0.00;
+    tipSum.innerHTML = "$" + "0.00";
+    totalSum.innerHTML = "$" + "0.00";
+    billInput.value = "0.0";
+    peopleInput.value = "1";
+    console.log("Splitter has been reseted");
 }
