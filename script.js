f4cdfb1531b05dc3295f982d5381a265f51277c1
albertseiler <113704524+albertseiler@users.noreply.github.com>
2023-02-21 18:50:28 +0100

    .

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
 //Javascript
-const billAmount = document.querySelector('.inputbill')
-const numberOfPeople = document.querySelector('.inputpeople')
-const percentageButtons = document.querySelectorAll('.tippercentage')
-const tipSum = document.querySelector('.dollaramount one')
-const totalSum = document.querySelector('.dollaramount two')
+const billInput = document.querySelector('.inputbill')
+const peopleInput = document.querySelector('.inputpeople')
+const tipSum = document.getElementById("tip-amount")
+const totalSum = document.getElementById("total-amount")
+const tips = document.querySelectorAll(".tippercentage")
 
-
-
-//addEventListeners
-
-percentageButtons.forEach((button, index) => {
-    button.addEventListener('click', () => getValueOfButton);
-
+billInput.addEventListener("input", billInputFun);
+peopleInput.addEventListener("input", peopleInputFun);
+tips.forEach(function (val) {
+    val.addEventListener('click', handleClick)
 })
 
+billInput.value = "0.0"
+peopleInput.value = "1"
+tipSum.innerHTML = "$" + (0.0).toFixed(2)
+totalSum.innerHTML = "$" + (0.0).toFixed(2)
 
-//Functions
-function getValueOfButton() {
-    const val = document.querySelectorAll('button').value;
-    console.log(val)
+let billValue = 0.0;
+let peopleValue = 1;
+let tipValue = 0.15;
+
+function billInputFun() {
+    billValue = parseFloat(billInput.value);
+    calculateTip();
 }
 
-function tipCalc(billAmount, numberOfPeople, percentageButtons) {
-    return (billAmount.value * (percentageButtons / 100) / numberOfPeople)
-    console.log("tip calculation")
-
+function peopleInputFun() {
+    peopleValue = parseFloat(peopleInput.value);
+    calculateTip();
 }
 
+function handleClick(event) {
+    tips.forEach(function (val) {
+        val.classList.remove("active-tip");
+        if (event.target.innerHTML == val.innerHTML) {
+            val.classList.add("active-tip");
+            tipValue = parseFloat(val.innerHTML) / 100
+        }
+    });
+    calculateTip();
+}
 
-
-function totalCalc(billAmount, tipSum, numberOfPeople) {
-    return ((billAmount + tipSum) / numberOfPeople)
-    console.log("total calculation")
+function calculateTip() {
+    if (peopleValue >= 1) {
+        let tipSum = (billValue * tipValue) / peopleValue;
+        let totalSum = (billValue * tipSum) / peopleValue;
+        tipSum.innerHTML = "$" + tipSum.toFixed(2);
+        totalSum.innerHTML = "$" + totalSum.toFixed(2);
+        console.log((billValue * tipValue) / peopleValue)
+    }
 }
